bf2914c84d5a26ce595d97fb55272e4ee07b634f
osiaeg <egor.goncharov.osia@mail.ru>
2023-02-10 19:12:10 +0300

    Init commit

---
+++ file_system_iterator.py
@@ -0,0 +1,17 @@
+class FileSystemIterator:
+
+    def __init__(self, root: str, only_files: bool, only_dirs: bool, pattern=None):
+        '''
+        Инициализация объекта
+
+        :param root:
+        :param only_files:
+        :param only_dirs:
+        :param pattern:
+        '''
+        pass
+
+if __name__ == '__main__':
+    print('Hello world')
+#    for item in FileSystemIterator('/tmp', False, False, None):
+#        print(item)
